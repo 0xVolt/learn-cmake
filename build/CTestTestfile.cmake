9dc6ff4b262c3b4f46d2e1522da6d6440e598d09
Desh Iyer <desh.iyer@gmail.com>
2022-11-30 14:34:07 +0530

    Generated build files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/deshi/Code/learn-cmake
+# Build directory: C:/Users/deshi/Code/learn-cmake/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
