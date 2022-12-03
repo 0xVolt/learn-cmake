03b48e97ee69309aae67eeaacd94b11fbc9cdd70
Desh Iyer <desh.iyer@gmail.com>
2022-12-04 01:32:48 +0530

    Added header file

---
+++ include/Common.h
@@ -0,0 +1,5 @@
+// To use the std::string data type
+#include <string>
+
+// Here is the function declaration whose implementation will be in a different source file
+void printFromSrc(std::string);
